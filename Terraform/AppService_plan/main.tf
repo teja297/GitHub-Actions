259295dfd1e849bec3b27eb1fd5db9e4ce0762c0
Jangam Teja <jangam.teja@dreamorbit.in>
2025-10-29 00:28:28 +0530

    updated yaml code 3

--- Terraform/AppService_plan/main.tf
+++ Terraform/AppService_plan/main.tf
@@ -3,16 +3,13 @@ data "azurerm_resource_group" "example" {
   name = var.resource_group_name
 }
 
-resource "azurerm_app_service_plan" "app_plan" {
+resource "azurerm_service_plan" "app_plan" {
   name                = var.app_service_plan_name
   location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
-  kind                = var.kind
-  reserved            = var.reserved  # true for Linux, false for Windows
+  os_type             = var.os_type           # "Linux" or "Windows"
+  sku_name            = var.sku_size        # e.g., "B1", "S1", "P1v2"
 
-  sku {
-    tier = var.sku_tier
-    size = var.sku_size
-  }
 
 }
+
--- Terraform/AppService_plan/variables.tf
+++ Terraform/AppService_plan/variables.tf
@@ -10,11 +10,11 @@ variable "app_service_plan_name" {
   default     = "my-app-service-plan"
 }
 
-variable "kind" {
-  description = "Type of App Service (Windows or Linux)"
-  type        = string
-  default     = "Linux"
-}
+# variable "kind" {
+#   description = "Type of App Service (Windows or Linux)"
+#   type        = string
+#   default     = "Linux"
+# }
 
 variable "reserved" {
   description = "Set to true for Linux plans"
@@ -22,15 +22,22 @@ variable "reserved" {
   default     = true
 }
 
-variable "sku_tier" {
-  description = "Pricing tier (e.g., Free, Basic, Standard, Premium)"
-  type        = string
-  default     = "Basic"
-}
+# variable "sku_tier" {
+#   description = "Pricing tier (e.g., Free, Basic, Standard, Premium)"
+#   type        = string
+#   default     = "Basic"
+# }
 
 variable "sku_size" {
   description = "Instance size (e.g., B1, S1, P1v2)"
   type        = string
   default     = "B1"
 }
+
+variable "os_type" {
+  description = "Operating system for the App Service Plan (Linux or Windows)"
+  type        = string
+  default     = "Linux"
+}
+
 
