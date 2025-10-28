fc6362535b2cbf8c86e66d2282771659365969ca
Jangam Teja <jangam.teja@dreamorbit.in>
2025-10-28 23:05:06 +0530

    updated the storrage account variables.tf

--- Terraform/storage_account/variables.tf
+++ Terraform/storage_account/variables.tf
@@ -7,6 +7,7 @@ variable "storage_account_name" {
 variable "resource_group_name" {
   type        = string
   description = "Name of the resource group"
+  default = "rg-dev-app"
 }
 
 variable "location" {
