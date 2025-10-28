1672e62b77f4bd754515f90b3316b87b4bfbc02a
Jangam Teja <jangam.teja@dreamorbit.in>
2025-10-29 02:48:01 +0530

    updated yaml code 15

--- Terraform/AppService_plan/main.tf
+++ Terraform/AppService_plan/main.tf
@@ -5,8 +5,8 @@ data "azurerm_resource_group" "example" {
 
 resource "azurerm_app_service_plan" "example" {
  name = "example-app-service-plan"
- location = azurerm_resource_group.example.location
- resource_group_name = azurerm_resource_group.example.name
+ location = data.azurerm_resource_group.example
+ resource_group_name = data.azurerm_resource_group.example
  sku {
    tier = "Standard"
    size = "S1"
