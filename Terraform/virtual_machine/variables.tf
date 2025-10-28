5b15d1da3aa5a3a67e78ac20340b79a4b2b71591
Jangam Teja <jangam.teja@dreamorbit.in>
2025-10-28 10:20:16 +0530

    updated the variables.tf

--- Terraform/virtual_machine/variables.tf
+++ Terraform/virtual_machine/variables.tf
@@ -1,11 +1,13 @@
 variable "subscription_id" {
   description = "Azure subscription ID"
   type        = string
+  default = "d051b475-bd0f-4c97-a366-27c880ee5c45"
 }
 
 variable "client_id" {
   description = "Azure client ID (Application ID)"
   type        = string
+  default = "ae391762-9ba8-4cb3-8721-6b4c7bb6bcea"
 }
 
 variable "client_secret" {
@@ -17,6 +19,7 @@ variable "client_secret" {
 variable "tenant_id" {
   description = "Azure tenant ID"
   type        = string
+  default = "e91cf59b-64d5-4c68-8cf5-7b5f9d91ac9d"
 }
 
 variable "location" {
