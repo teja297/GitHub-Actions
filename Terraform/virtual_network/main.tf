f63ee24929b4a18f97e7f6d7aa4bb64ad232f340
Jangam Teja <jangam.teja@dreamorbit.in>
2025-10-28 18:37:11 +0530

    updated the yaml code 01

--- Terraform/virtual_network/main.tf
+++ Terraform/virtual_network/main.tf
@@ -1,13 +1,18 @@
+
+data "azurerm_resource_group" "example" {
+  name = "rg-dev-app"
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = [var.vnet_address_space]
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = data.azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "private_subnet" {
   name                 = var.subnet_name
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = data.azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet_prefix]
 }
