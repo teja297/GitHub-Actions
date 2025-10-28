03574e8b66e5a1fe0f5f57ec7e8de48e5816f0b3
Jangam Teja <jangam.teja@dreamorbit.in>
2025-10-29 02:43:37 +0530

    updated yaml code 14 updated the plan code

--- Terraform/AppService_plan/main.tf
+++ Terraform/AppService_plan/main.tf
@@ -3,13 +3,13 @@ data "azurerm_resource_group" "example" {
   name = var.resource_group_name
 }
 
-resource "azurerm_service_plan" "app_plan" {
-  name                = var.app_service_plan_name
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
-  os_type             = var.os_type           # "Linux" or "Windows"
-  sku_name            = var.sku_size        # e.g., "B1", "S1", "P1v2"
-
-
+resource "azurerm_app_service_plan" "example" {
+ name = "example-app-service-plan"
+ location = azurerm_resource_group.example.location
+ resource_group_name = azurerm_resource_group.example.name
+ sku {
+   tier = "Standard"
+   size = "S1"
+ }
 }
 
