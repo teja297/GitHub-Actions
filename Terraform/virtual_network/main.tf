003f67ad40feccf933e4d5cd9e698bcfe0c5fe35
Jangam Teja <jangam.teja@dreamorbit.in>
2025-10-28 18:47:46 +0530

    updated the yaml code 02

--- Terraform/virtual_network/main.tf
+++ Terraform/virtual_network/main.tf
@@ -6,7 +6,7 @@ data "azurerm_resource_group" "example" {
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = [var.vnet_address_space]
-  location            = var.location
+  location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
 }
 
