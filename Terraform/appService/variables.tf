eac3b4a54705ba83dadf80671aa1b40a31a8110a
Jangam Teja <jangam.teja@dreamorbit.in>
2025-10-28 23:31:35 +0530

    added the code for app service plan

---
+++ Terraform/appService/variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default = "rg-dev-app"
+}
+
+variable "app_service_plan_name" {
+  description = "Name of the App Service Plan"
+  type        = string
+  default = "my-app-service-plan"
+}
+
+variable "app_service_name" {
+  description = "Name of the App Service (Web App)"
+  type        = string
+  default     = "my-web-app"
+}
+
+variable "linux_fx_version" {
+  description = "Runtime stack for Linux (e.g., PYTHON|3.10, NODE|18-lts)"
+  type        = string
+  default     = "PYTHON|3.10"
+}
+
