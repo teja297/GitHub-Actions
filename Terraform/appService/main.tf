1ba655adb02f9c633d7dc0d4fb5df452ba321e56
Jangam Teja <jangam.teja@dreamorbit.in>
2025-10-29 15:06:25 +0530

    added the app service plan code into appservice 02 removed the data block in main.tf

--- Terraform/appService/main.tf
+++ Terraform/appService/main.tf
@@ -3,34 +3,29 @@ data "azurerm_resource_group" "example" {
   name = var.resource_group_name
 }
 
-data "azurerm_service_plan" "example" {
-  name                = var.app_service_plan_name
-  resource_group_name = var.resource_group_name
-}
 
 
 resource "azurerm_app_service_plan" "example" {
- name = var.app_service_plan_name
- location = data.azurerm_resource_group.example.location
- resource_group_name = data.azurerm_resource_group.example.name
- sku {
-   tier = "Standard"
-   size = "S1"
- }
+  name                = var.app_service_plan_name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
 }
 
 resource "azurerm_app_service" "webapp" {
   name                = var.app_service_name
   location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
-  app_service_plan_id = data.azurerm_service_plan.example.id
-
+  app_service_plan_id = azurerm_app_service_plan.example.id
 
   site_config {
-    always_on = true
-    linux_fx_version = var.linux_fx_version  # Example: "PYTHON|3.10" or "NODE|18-lts"
+    always_on        = true
+    linux_fx_version = var.linux_fx_version
   }
 
   https_only = true
-
 }
+
